cfa4441089d2beead6343d102ba5242f92cd8716
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-08-09 13:09:45 +1000

    Progress that was made during the workshop last week. CODE NEEDS FIXING

--- skeletonMaze.go
+++ skeletonMaze.go
@@ -1,7 +1,37 @@
 package main
 
+import "fmt"
+
+// Struct for Maze
+type maze struct {
+	maze [][] string 
+	xSize int
+	ySize int
+}
+
+type icon struct {
+	xPos int
+	yPos int
+	icon string
+}
+
+type player struct {
+	image icon
+}
+
+type exit struct {
+	image icon
+}
+
+type key struct {
+	image icon
+}
+
+
 func main() {
-	mazeMap := [][]string{
+
+	// 2D String Slice for Map 🎯
+	mazeMap := [][] string {
 		{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
 		{"0", " ", " ", " ", " ", " ", "0", " ", " ", " ", "0"},
 		{"0", "0", "0", "0", "0", " ", "0", "0", "0", " ", "0"},
@@ -15,16 +45,107 @@ func main() {
 		{"0", "0", "0", "0", "0", " ", "0", "0", "0", "0", "0"},
 	}
 
+	gameMaze := maze {
+		maze: mazeMap,
+		xSize: len(mazeMap),
+		ySize: len(mazeMap),
+	}
+
+	exit := exit {
+		image: icon { 
+			xPos: 5,
+			yPos: len(mazeMap),
+			icon: "E",
+		},
+	}
+
+	key := key {
+		image: icon { 
+			xPos: 1,
+			yPos: 1,
+			icon: "K", 
+		},
+	}
+
+	// Create the player
+	player := newPlayer(5, 1)
+
+	// Game loop
+	running := true
+	printables := [] *icon {&player.image, &key.image, &exit.image}
+	for running {
+		draw(&gameMaze, printables)
+	}
 }
 
-func newPlayer() {
-	//TODO
+// This function takes in a position and returns a player instance
+func newPlayer(xPos int, yPos int) *player {
+	p := player {
+		image: icon { 
+			xPos: xPos,
+			yPos: yPos,
+			icon: "P",
+		},
+	}
+
+	return &p
 }
 
-func draw() {
-	//TODO
+func draw(gameMazeTrue *maze, icons [] *icon) {
+
+	var gameMaze maze
+
+	gameMaze.maze = make([][] string, len(gameMazeTrue.maze))
+
+	for i := gameMazeTrue.maze {
+		gameMaze.maze[i] = make([] string, len(gameMazeTrue.maze[i]))
+
+		copy(gameMaze.maze[i], gameMazeTrue.maze[i])
+	}
+	
+	for _, s := range icons {
+		gameMaze.maze[s.yPos][s.xPos] = s.icon
+	}
+
+	for row := range gameMaze.maze {
+		for col := range gameMaze.maze[row] {
+			print(gameMaze.maze[row][col])
+		}
+
+		// New line end of every row
+		print("\n")
+	}
 }
 
-func move() {
-	//TODO
+func move(gameMaze *maze, player *player) {
+
+
+	var i string
+
+	// Initialise get input
+	getInput := true
+
+	// getInput loop
+	for getInput {
+
+		// Start scanner
+		fmt.Scan(&i)
+
+		// Start at false
+		getInput = false
+
+		// Movement input checking
+		if i == "w" {
+			player.image.yPos -= 1
+		} else if i == "a" {
+			player.image.xPos -= 1
+		} else if i == "s" {
+			player.image.yPos += 1
+		} else if i == "d" { 
+			player.image.xPos += 1
+		} else {
+			// If we get no inputs, set it back to true so it asks again
+			getInput = true
+		}
+	}
 }
